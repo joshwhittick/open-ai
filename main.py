e2ccfe6b15e910f540b4b2d71c47d9d450f13999
joshwhittick <92024297+joshwhittick@users.noreply.github.com>
2022-09-15 14:50:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ openai.api_key=api_data
 completion=openai.Completion()
 
 def Reply(question):
-	prompt=f'Josh: {question}\n Jarvis'
-	response=completion.create(prompt=prompt, engine="text-davinci-002", stop=['\Josh'], max_tokens=300)
+	prompt=f'Josh: {question} OpenAI\n'
+	response=completion.create(prompt=prompt, engine="text-davinci-002", max_tokens=300)
 	answer=response.choices[0].text.strip()
 	return answer
 
-
 ans = Reply(input("Enter promt for OpenAI:" ))
-
 print(ans)
