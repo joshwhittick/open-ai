f0832bbcf5a5a605f9ee6084a5ce3953c822abc9
Josh Whittick <92024297+joshwhittick@users.noreply.github.com>
2022-11-17 16:29:47 +0000

    Update run_AI.zsh

--- run_AI.zsh
+++ run_AI.zsh
@@ -3,7 +3,7 @@ do
 	echo "Do you want to query OpenAI (y/n)?"
 	read inp
 	if [ $inp == "y" ]; then 
-		python3 /PATH/TO/FOLDER/open_AI/AI_with_audio_input_option.py
+		python3 /PATH/TO/FOLDER/open_AI/AI_with_audio_input.py
 	elif [ $inp == "n" ]; then 
 		exit
 	elif [ $inp != "y" ] | [ $inp != "n" ]; then
